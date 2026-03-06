db2892ae4b9bde4e89617702711aceca3dc33eb5
Joelviz <102918375+Joelviz@users.noreply.github.com>
2026-03-06 10:53:17 +0100

    Create App.jsx main application component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FitnessApp from './FitnessApp';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <FitnessApp />
+        </div>
+    );
+}
+
+export default App;
